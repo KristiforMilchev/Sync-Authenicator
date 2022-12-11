7a27fcece7d0d4c31ebe7e41a60df9f9218747d0
Kristifor Milchev <kristifor@collaborativesoft.com>
2022-12-11 14:25:18 +0200

    Moving the tests into separate groups cleaning up the test view tree

--- test/block_provider/block_provider_test.dart
+++ test/block_provider/block_provider_test.dart
@@ -40,55 +40,57 @@ void main() {
         expect(account, isNot(null),
             reason: "Account failed to recover credentials with private key");
       });
+    });
+  });
 
-      test("Encrypt account private key", () {
-        var privateKey = TestsData().privateKey;
-        var encryptionKey = TestsData().encryptionPassword;
+  group("Encryption Handling", () {
+    test("Encrypt account private key", () {
+      var privateKey = TestsData().privateKey;
+      var encryptionKey = TestsData().encryptionPassword;
 
-        var encrypted = provider.encryptPk(encryptionKey, privateKey);
+      var encrypted = provider.encryptPk(encryptionKey, privateKey);
 
-        expect(encrypted, isNot(null),
-            reason: "failed to encrypt private key, returned null");
-      });
+      expect(encrypted, isNot(null),
+          reason: "failed to encrypt private key, returned null");
+    });
 
-      test("Recover encrypted private key", () {
-        var encrypted = Encrypted.fromBase64(TestsData().derivedBase64EncData);
-        var encryptionKey = TestsData().encryptionPassword;
-        var originalWallet = TestsData().initTestWallet();
+    test("Recover encrypted private key", () {
+      var encrypted = Encrypted.fromBase64(TestsData().derivedBase64EncData);
+      var encryptionKey = TestsData().encryptionPassword;
+      var originalWallet = TestsData().initTestWallet();
 
-        var decrypt = provider.getAccount(encryptionKey, encrypted);
+      var decrypt = provider.getAccount(encryptionKey, encrypted);
 
-        expect(decrypt.publicAddress.toLowerCase(),
-            originalWallet.publicAddress.toLowerCase(),
-            reason: "Wallet recovered using encrypted private key");
-      });
+      expect(decrypt.publicAddress.toLowerCase(),
+          originalWallet.publicAddress.toLowerCase(),
+          reason: "Wallet recovered using encrypted private key");
+    });
 
-      test("Sign simple messsage", () {
-        var originalWallet = TestsData().initTestWallet();
+    test("Sign simple messsage", () {
+      var originalWallet = TestsData().initTestWallet();
 
-        var generatedSingnature =
-            provider.signData(TestsData().testMessage, originalWallet);
+      var generatedSingnature =
+          provider.signData(TestsData().testMessage, originalWallet);
 
-        expect(generatedSingnature, isNot(null), reason: "Signature is null");
-        expect(generatedSingnature, isNot(""),
-            reason: "Signature is whitespace or empty");
-      });
+      expect(generatedSingnature, isNot(null), reason: "Signature is null");
+      expect(generatedSingnature, isNot(""),
+          reason: "Signature is whitespace or empty");
+    });
 
-      test("Decode signed message", () {
-        var originalWallet = TestsData().initTestWallet();
+    test("Decode signed message", () {
+      var originalWallet = TestsData().initTestWallet();
 
-        var msg = TestsData().recordedTestSginature;
-        var newRandomWallet = provider.createNewAccount();
+      var msg = TestsData().recordedTestSginature;
+      var newRandomWallet = provider.createNewAccount();
 
-        var owner = provider.decodeSigned(msg, TestsData().testMessage);
+      var owner = provider.decodeSigned(msg, TestsData().testMessage);
 
-        expect(originalWallet.publicAddress.toLowerCase(), owner.toLowerCase(),
-            reason:
-                "Original creator doesn't derive from signed message after decoding");
-        expect(owner.toLowerCase(),
-            isNot(newRandomWallet.address.toString().toLowerCase()),
-            reason: "doesn't match random address");
-      });
+      expect(originalWallet.publicAddress.toLowerCase(), owner.toLowerCase(),
+          reason:
+              "Original creator doesn't derive from signed message after decoding");
+      expect(owner.toLowerCase(),
+          isNot(newRandomWallet.address.toString().toLowerCase()),
+          reason: "doesn't match random address");
     });
   });
 }
